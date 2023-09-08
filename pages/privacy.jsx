f9eb187a66e6aa1c4ae2b05ddd2909271eb3e659
cihat04 <144404165+cihat04@users.noreply.github.com>
2023-09-08 09:08:50 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -2,21 +2,21 @@ export default function Privacy() {
 
     return (
         <div>
-            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Astra</h1>
+            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Pyremix</h1>
             <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ To use the Astra bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
+  <p>⊳ Botu Sunucuya Eklerken Bütün İzinlerin Açık Olmasına dikkat ediniz. <a className="text-white">(Discord Sunucumuzda Bize Destek Olmayı Unutmayın.)</a></p>
         <div className="py-3"></div>
-        <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/924325576095973426/vote">Astra</a> bot when using certain commands from the bot commands.</p>
+        <p>⊳ Bota oy ver <a className="text-white" href="https://Yakında">Pyremix</a> .</p>
         <div className="py-3"></div>
-        <p>⊳ Every user can use the astra bot however they want, as long as they do not intend to harm.</p>
+        <p>⊳ Her kullanıcı Pyremix botu zarar verme kastı olmadığı sürece dilediği gibi kullanabilir.</p>
         <div className="py-3"></div>
-        <p>⊳ You cannot use certain commands at the same time while using Astra.</p>   
+        <p>⊳ Pyremixi kullanırken belirli komutları aynı anda kullanamazsınız.</p>   
         <div className="py-3"></div>
-        <p>⊳ Astra bot does not save any data based on users.</p>
+        <p>⊳ Pyremix bot kullanıcılara dayalı hiçbir veri kaydetmez.</p>
         <div className="py-3"></div>
-        <p>⊳ While using the bot, your user ID is saved and deleted in the system for 60 seconds for once.</p> 
-    
+        <p>⊳ Bot size ayit hiçbir veriyi kaydetmez ve sızdırmaz.</p> 
+
         </div>
             </div>
     );
